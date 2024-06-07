002593a33d53bcb507db99993c15ec9994b7fa57
filip <filip.vorisek@volny.cz>
2024-06-07 09:26:24 +0200

    change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Home.css"
+import { useState, useRef } from 'react'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function Home() {
+
+const [color, setColor]=useState();
+
+const myDiv = useRef();
+
+const handleColorChange = (e) => {
+  setColor(e.target.value);
+}
+
+const handleClick = () => {
+  myDiv.current.style.backgroundColor = color;
+}
+
+
   return (
     <>
-    <div>Home</div>
+    <input type="text" onChange={handleColorChange}/>
+    <button onClick={handleClick}>Change color</button>
+    <div ref={myDiv} className='home-foo'>Home</div>
     <Link to={"/signin"}>
     <button>Go to sign in</button>
     </Link>
+    <Link to={"/aboutus"}>
+    <button>about us</button>
+    </Link>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  
     
     </>
   )
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,39 +1,51 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { useState,useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function SignIn() {
-    const [formData, setFormData] = useState({
-        email: "",
-        password: ""
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e) =>{
-        setFormData({...formData, [e.target.name]: e.target.value});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  const handelSubmit = (e) => {
+    if (formData.email.length > 0 && formData.password.length > 0) {
+      console.log("Signed in!");
+      navigate("/");
     }
-
-    useEffect(()=>{
-        console.log(formData);
-    }, [formData])
-
-    const handelSubmit = (e) =>{  
-        if (formData.email.length > 0 && formData.password.length > 0){
-        console.log("Signed in!")
-            navigate("/")
-        }
-    }
-    const navigate = useNavigate();
+  };
+  const navigate = useNavigate();
 
   return (
     <>
-    <div>Sign in</div>
-    <form onSubmit={(e)=>e.preventDefault()}>
-        <input required type="email" placeholder='Enter your email' name='email' onChange={handleChange}/>
-        <input required type="password" placeholder='Enter your password' name='password' onChange={handleChange}/>
-        <input type="submit" value={"sign in"} onClick={handelSubmit}/>
-    </form>
-    <Link to={"/"}>
-    <button>Back to home</button>
-    </Link>
+      <div>Sign in</div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          required
+          type="email"
+          placeholder="Enter your email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Enter your password"
+          name="password"
+          onChange={handleChange}
+        />
+        <input type="submit" value={"sign in"} onClick={handelSubmit} />
+      </form>
+      <Link to={"/"}>
+        <button>Back to home</button>
+      </Link>
     </>
-  )
+  );
 }
