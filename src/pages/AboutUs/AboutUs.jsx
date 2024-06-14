c8339f913b0d4efa64f4510ed9d20c2a11d0c3c0
filip <filip.vorisek@volny.cz>
2024-06-14 09:25:32 +0200

    changes

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Slider from '@mui/material/Slider';
-import VolumeDown from '@mui/icons-material/VolumeDown';
-import VolumeUp from '@mui/icons-material/VolumeUp';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
-
-    
-  
 export default function AboutUs() {
-    const [value, setValue] = React.useState(30);
-  
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    }
   return (
     <>
-    <Box sx={{ width: 200 }}>
-      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-        <VolumeDown />
-        <Slider aria-label="Volume" value={value} onChange={handleChange} />
-        <VolumeUp />
-      </Stack>
-      <Slider disabled defaultValue={30} aria-label="Disabled slider" />
-    </Box>
-    <Link to={"/"}>
-    <button>back to home</button>
-    </Link>
+      <Box sx={{ width: "100%" }}>
+        <Stack
+          direction="column-reverse"
+          justifyContent="flex-end"
+          alignItems="stretch"
+          spacing={1}
+        >
+          <Item>
+            <Link to={"/"}>
+              <button>back to home</button>
+            </Link>
+          </Item>
+        </Stack>
+      </Box>
     </>
-  )
-
+  );
 }
-
-
--- src/pages/AppRoutes.jsx
+++ src/pages/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './Home/Home'
 import SignIn from './SignIn/SignIn'
+import AboutUs from './AboutUs/AboutUs'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/aboutus' element={<AboutUs/>}/>
         <Route path='/signin' element={<SignIn/>}/>
     </Routes>
     </BrowserRouter>
